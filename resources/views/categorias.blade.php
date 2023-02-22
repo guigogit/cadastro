d06351e717afaa7d8767c5e46736fb64045bed5b
rodrigo <rodrigo.oliveira@spyonsky.com.br>
2023-02-22 10:20:31 -0300

    Aula 72

---
+++ resources/views/categorias.blade.php
@@ -0,0 +1,6 @@
+@extends('layout.app')
+
+@section('body')
+    <h4>Página de categorias</h4>
+@endsection
+
---
+++ resources/views/produtos.blade.php
@@ -0,0 +1,6 @@
+@extends('layout.app')
+
+@section('body')
+    <h4>Página de produtos</h4>
+@endsection
+
