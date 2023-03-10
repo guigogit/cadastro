4e5e35e341575e47c54174c835675633c527bd38
rodrigo <rodrigo.oliveira@spyonsky.com.br>
2023-03-10 15:43:43 -0300

    Aula_98

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -1,31 +1,42 @@
 <html>
     <head>
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-        <title> Cadastro de Produtos</title>
+        <!-- jQuery -->
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+
+<!-- Bootstrap -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.js"></script>
+
+
+        <title>Cadastro de Produtos</title>
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <style>
             body {
                 padding: 20px;
             }
-            .navbar{
+            .navbar {
                 margin-bottom: 20px;
             }
-
-
         </style>
-
     </head>
-    <body>
-        <div class="container">
-            @component('componente_navbar', ["current" => $current])
+<body>
+    <div class="container">
+        @component('componente_navbar', [ "current" => $current ])
+        @endcomponent
+        <main role="main">
+            @hasSection('body')
+                @yield('body')
+            @endif
+        </main>
+    </div>
 
-            @endcomponent
-            <main role="main">
-                @hasSection('body')
-                    @yield('body')
-                @endif
-        </div>
 
-        <script src = "{{ asset('js/app.js') }}" type="text/javascript"></script>
-    </body>
+    <script src="{{ asset('js/app.js')}}" type="text/javascript"></script>
+
+
+    @hasSection('javascript')
+        @yield('javascript')
+    @endif
+</body>
 </html>
